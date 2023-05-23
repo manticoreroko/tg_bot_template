47bb49044680e446b338ba382cefd82498776bc4
Pavel K <svodnebes@mail.ru>
2023-05-23 22:21:02 +0300

    ok

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from aiogram import Bot, executor, Dispatcher, types
 from aiogram.types import ReplyKeyboardRemove
 
+from keyboards.command_menu import set_main_menu
+
 async def on_startup(_):
     await bot.send_message(chat_id=MAIN_ID, text='Бот запущен', reply_markup=ReplyKeyboardRemove())
     await set_main_menu(dp=dp)
